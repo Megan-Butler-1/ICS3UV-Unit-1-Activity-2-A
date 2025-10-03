eb4afabddeb61952669bcdff14a234b02ee2a467
default <megan.butler@stu.ocsb.ca>
2025-10-03 13:38:28 +0000

    added comments

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Megan Butler
+ * @version 1.0.0
+ * @date 2025-10-03
+ * @fileoverview This is the Hello World program
+ */
+
+//print out the greeting
+console.log("Hello, World!");
+
+console.log("\nDone.")
